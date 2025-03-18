cf987a23ab9f38be223513e5d56ec9fdb0cd61ea
malik01 <www.maliksk@gmail.com>
2025-03-18 23:44:25 +0530

    change

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,10 @@
 from flask import jsonify
 from flask_jwt_extended import get_jwt_identity
-
+from functools import wraps  
 
 def role_required(role):
     def decorator(fn):
+        @wraps(fn)  
         def wrapper(*args, **kwargs):
             user = get_jwt_identity()
             if user['role'] != role:
